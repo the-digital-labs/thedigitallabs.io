a67fb7fb4d6c5da5330c4b3c4c3fad86bc3fb070
Chris <chris.eddy.dev@gmail.com>
2020-10-22 00:42:32 -0700

    navbar, dark mode styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import 'fontsource-roboto';
+import Nav from './components/Nav.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />
     </div>
   );
 }
