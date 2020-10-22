25e2bb6fe90e2bbbdb356e6fcedb1d1177c22678
Chris <chris.eddy.dev@gmail.com>
2020-10-22 14:29:51 -0700

    frontpage video overlay work

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import mountainVideo from '../mountain.mp4';
 
 const useStyles = makeStyles((theme) => ({
   frontPage: {
     height: '100vh',
-  }
+  },
+  video: {
+    height: '100%',
+    width: '100%',
+    opacity: '60%',
+    objectFit: 'cover',
+  },
+  overlay: {
+    width: '100%',
+    position: 'absolute',
+    top: '40%',
+    textAlign: 'center',
+    color: '#ffadbb',
+    fontSize: 50
+  },
 }));
 
 const FrontPage = (props) => {
   const classes = useStyles();
   return (
     <div className={classes.frontPage}>
-      
+      <video className={classes.video} controls={false} autoPlay loop muted>
+        <source src={mountainVideo} type="video/mp4" />
+      </video>
+      <div className={classes.overlay}>
+        <h1>The Digital Labs</h1>
+      </div>
     </div>
   );
 }
