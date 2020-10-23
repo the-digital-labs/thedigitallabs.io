cf78c44d0047a77578f8099ba5e8bb279ef7201c
Chris <chris.eddy.dev@gmail.com>
2020-10-22 18:44:47 -0700

    nav side drawer

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Logo from '../media/logo.jpg';
+import NavDrawer from './NavDrawer.js'
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -27,19 +28,23 @@ const useStyles = makeStyles((theme) => ({
 
 const Nav = (props) => {
   const classes = useStyles();
+  const [drawerVisible, setDrawerVisible] = useState(false);
   return (
-    <AppBar position="static" className={classes.appBar}>
-      <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <img className={classes.logo} src={Logo} alt="logo" />
-        <Typography variant="h6" className={classes.title}>
-          The Digital Labs
-        </Typography>
-        <Button color="inherit">Contact</Button>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={(e) => setDrawerVisible(!drawerVisible)}>
+            <MenuIcon />
+          </IconButton>
+          <img className={classes.logo} src={Logo} alt="logo" />
+          <Typography variant="h6" className={classes.title}>
+            The Digital Labs
+          </Typography>
+          <Button color="inherit">Contact</Button>
+        </Toolbar>
+      </AppBar>
+      <NavDrawer visible={drawerVisible} closeDrawer={(e) => setDrawerVisible(false)} />
+    </div>
   );
 }
 export default Nav;
---
+++ src/components/NavDrawer.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import GroupIcon from '@material-ui/icons/Group';
+import StarsIcon from '@material-ui/icons/Stars';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
+
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    '& .MuiDrawer-paperAnchorLeft': {
+      backgroundColor: '#212121',
+      color: 'white',
+      '& .MuiSvgIcon-root': {
+        color: '#ff9de1'
+      }
+    }
+  },
+  list: {
+    width: 250
+  }
+}));
+
+const listItems = [
+  {
+    text: 'Services',
+    icon: <StorefrontIcon />
+  },
+  {
+    text: 'Team',
+    icon: <GroupIcon />
+  },
+  {
+    text: 'Clients',
+    icon: <StarsIcon />
+  },
+  {
+    text: 'Contact',
+    icon: <ContactSupportIcon />
+  },
+]
+
+const NavDrawer = (props) => {
+  const classes = useStyles();
+  return (
+      <Drawer className={classes.drawer} anchor='left' open={props.visible} onClose={(e) => props.closeDrawer()}>
+        <List className={classes.list}>
+          {listItems.map((item, index) => (
+            <ListItem button key={item.text}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+  );
+}
+export default NavDrawer;
