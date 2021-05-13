618ee1e70a48dcf6b9a97a498fce613233efe962
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-05-12 22:25:43 -0700

    remove taj from team

--- src/components/Team.js
+++ src/components/Team.js
@@ -4,10 +4,8 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import chris from '../media/chris1.jpg';
 import sylvia from '../media/sylvia1.jpg';
-import taj from '../media/taj.jpg'
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import InstagramIcon from '@material-ui/icons/Instagram';
-import TwitterIcon from '@material-ui/icons/Twitter';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import '../styles/team.css';
 
@@ -93,17 +91,11 @@ const Team = (props) => {
               <img className={classes.profileImg + ' team-profile-image'} src={chris} alt="chris-eddy"/>
             </Grid>
             <Grid item xl={9} lg={8} md={12} sm={12} xs={12}>
-              <Typography className={classes.profileName + ' team-profile-name'}>Christopher-Joseph Ferdinando Trimboli (Rookie Bleu)</Typography>
+              <Typography className={classes.profileName + ' team-profile-name'}>Christopher Trimboli</Typography>
               <Typography className={classes.profileTitle + ' team-profile-title'}>CEO | Web Engineer | Creator</Typography>
               <div className={classes.profileIcons}>
                 <a className={classes.profileIcon} href="https://www.linkedin.com/in/rookiebleu/" target="__blank">
                   <LinkedInIcon />
-                </a>
-                <a className={classes.profileIcon} href="https://www.instagram.com/rookie.bleu/" target="__blank">
-                  <InstagramIcon />
-                </a>
-                <a className={classes.profileIcon} href="https://twitter.com/RookieBleu" target="__blank">
-                  <TwitterIcon />
                 </a>
                 <a className={classes.profileIcon} href="https://github.com/RookieBleu" target="__blank">
                   <GitHubIcon />
@@ -128,7 +120,7 @@ const Team = (props) => {
               <img className={classes.profileImg + ' team-profile-image'} src={sylvia} alt="sylvia"/>
             </Grid>
             <Grid item xl={9} lg={8} md={12} sm={12} xs={12}>
-              <Typography className={classes.profileName + ' team-profile-name'}>Sylvia Makuch (Checkmeeeowt)</Typography>
+              <Typography className={classes.profileName + ' team-profile-name'}>Sylvia Makuch</Typography>
               <Typography className={classes.profileTitle + ' team-profile-title'} style={{color: '#ffc0e3'}}>Digital Artist | Stylist | Creative</Typography>
               <div className={classes.profileIcons}>
                 <a className={classes.profileIcon} href="https://www.linkedin.com/in/sylvia-makuch-6baa4a66/" target="__blank">
@@ -145,30 +137,6 @@ const Team = (props) => {
                 Her favorite ways of creating are: Procreate on iPad Pro w/ Apple Pen, Adobe programs, real canvas watercolors, paint and easel and is starting to learn Blender.
                 She does excellent interactive digital art with clients and can take your vision and make it reality on the canvas.
                 She has a warm and caring approach to working with people and is a business woman to watch out for. 
-                Currently, Sylvia works casual / part-time for The Digital Labs while attending 3D digital design / art school.
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-
-        <Grid className={classes.profileCard + ' team-profile'} item xl={12} lg={12} md={12} sm={12} xs={12}>
-          <Grid container>
-            <Grid item xl={3} lg={4} md={12} sm={12} xs={12}>
-              <img className={classes.profileImg + ' team-profile-image'} src={taj} alt="taj"/>
-            </Grid>
-            <Grid item xl={9} lg={8} md={12} sm={12} xs={12}>
-              <Typography className={classes.profileName + ' team-profile-name'}>Taj Isiah</Typography>
-              <Typography className={classes.profileTitle + ' team-profile-title'} style={{color: 'rgb(255 248 170)'}}>Videographer | Photographer | Productions</Typography>
-              <div className={classes.profileIcons}>
-                <a className={classes.profileIcon} href="https://www.instagram.com/tajisiah/" target="__blank">
-                  <InstagramIcon />
-                </a>
-              </div>
-              <hr className={classes.profileDivider}/>
-              <Typography className={classes.profileDescription + ' team-profile-description'}>
-                Taj is a keen, steady and creative production specialist. He is skilled with gimbles, lighting, DSL cameras, editing photos and video, creating music videos, short films, Instagram shots, modeling and content creation.
-                He brings a welcome hand to the creation and marketing process of any company. Taj is working on getting his pro level drone pilot license so that we can legally shoot in urban areas / weddings / crowded events. 
-                Currently, Taj works part-time for The Digital Labs while he attends some film classes and is teaching himself advanced Adobe skills.
               </Typography>
             </Grid>
           </Grid>
